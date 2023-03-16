baaa30c003d02cc7c92a448b131058c8ca1e1b0f
MichaelGreason <mgreason18@gmail.com>
2023-03-16 17:43:41 -0400

    added all contents to page via JSX, including button functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
 import './App.css';
+import  filmData  from './film-data.json';
+import { useState } from 'react';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='section'>
+      <div>
+        <div>
+      {
+        <Gallery/>
+       }
+        </div>
+      </div>
     </div>
-  );
+    );
 }
+
+function Gallery() {
+  let [index, setIndex] = useState(0);
+
+  function handleNextClick() {
+    setIndex(index + 1);
+    if (index > (filmData.length))
+    {
+      setIndex = 0
+    }
+  }
+  function handleBackClick() {
+    setIndex(index - 1)
+  }
+  function handleRestartClick() {
+    setIndex(0)
+  }
+
+  let data = filmData[index];
+  return (
+    <>
+    <button onClick={handleBackClick}>
+      Back
+    </button>
+    <button onClick={handleNextClick}>
+      Next
+      </button> 
+    <button onClick={handleRestartClick}>
+      Restart
+      </button> 
+      
+      <h1>
+        {data.title}
+      </h1>
+      <div>
+        <dive>
+          <img src={data.image} alt='cover art'></img>
+        </dive>
+      <h2>
+        {data.original_title}
+      </h2>
+      <div>
+        {data.release_date}
+      </div>
+      <p>{data.description}</p>
+      </div>
+      
+
+    </>
+  )
+}
+
+
 
 export default App;
