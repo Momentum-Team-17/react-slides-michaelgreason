791b69186454493b44ebf893d344d7261c89cdd2
MichaelGreason <mgreason18@gmail.com>
2023-03-17 11:40:49 -0400

    all buttons function properly, applying css via bootstrap

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <div>
       {
         <Gallery/>
-       }
+        }
         </div>
       </div>
     </div>
@@ -21,12 +21,8 @@ function Gallery() {
   let [index, setIndex] = useState(0);
 
   function handleNextClick() {
-    setIndex(index + 1);
-    if (index > (filmData.length))
-    {
-      setIndex = 0
-    }
-  }
+    setIndex(index + 1)
+}
   function handleBackClick() {
     setIndex(index - 1)
   }
@@ -37,31 +33,40 @@ function Gallery() {
   let data = filmData[index];
   return (
     <>
-    <button onClick={handleBackClick}>
-      Back
-    </button>
-    <button onClick={handleNextClick}>
-      Next
-      </button> 
-    <button onClick={handleRestartClick}>
-      Restart
-      </button> 
-      
+    <div className='all'>
       <h1>
         {data.title}
       </h1>
-      <div>
-        <dive>
-          <img src={data.image} alt='cover art'></img>
-        </dive>
-      <h2>
-        {data.original_title}
-      </h2>
-      <div>
-        {data.release_date}
+        <div class="card-group" className='below-title'>
+          <div>
+            <div class='card' className='image-box' width='80%' height='80%'>
+              <img class='card-body' className='image' src={data.image} alt='cover art'></img>
+            </div>
+            <div class='card' className='side-card'>
+              <div class='card-body'>
+              <h2>
+                {data.original_title}
+              </h2>
+              <div>
+                {data.release_date}
+              </div>
+                <p>{data.description}</p>
+                </div>
+            </div>  
+          </div>
+        </div>
       </div>
-      <p>{data.description}</p>
-      </div>
+    <div className='button-box'>
+        <button onClick={handleBackClick} disabled={index === 0}>
+          Back
+        </button>
+        <button onClick={handleNextClick} disabled={index === filmData.length - 1}>
+          Next
+          </button> 
+        <button onClick={handleRestartClick} disabled={index === 0}>
+          Restart
+          </button> 
+    </div>
       
 
     </>
