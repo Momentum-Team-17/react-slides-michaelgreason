3d2c5fb8ff4d132447596441f597600d142bcf29
MichaelGreason <mgreason18@gmail.com>
2023-03-17 13:36:21 -0400

    finished css styling. moving on to spicy options.

--- src/App.js
+++ src/App.js
@@ -38,8 +38,7 @@ function Gallery() {
         {data.title}
       </h1>
         <div class="card-group" className='below-title'>
-          <div>
-            <div class='card' className='image-box' width='80%' height='80%'>
+            <div class='card' className='image-box'>
               <img class='card-body' className='image' src={data.image} alt='cover art'></img>
             </div>
             <div class='card' className='side-card'>
@@ -47,23 +46,22 @@ function Gallery() {
               <h2>
                 {data.original_title}
               </h2>
-              <div>
+              <div className='rd' >
                 {data.release_date}
               </div>
-                <p>{data.description}</p>
+                <p class='des'>{data.description}</p>
                 </div>
             </div>  
-          </div>
         </div>
-      </div>
-    <div className='button-box'>
-        <button onClick={handleBackClick} disabled={index === 0}>
+    </div>
+    <div class='btn-group' className='button-box'>
+        <button class="btn btn-secondary" onClick={handleBackClick} disabled={index === 0}>
           Back
         </button>
-        <button onClick={handleNextClick} disabled={index === filmData.length - 1}>
+        <button class="btn btn-secondary" onClick={handleNextClick} disabled={index === filmData.length - 1}>
           Next
           </button> 
-        <button onClick={handleRestartClick} disabled={index === 0}>
+        <button class="btn btn-secondary" onClick={handleRestartClick} disabled={index === 0}>
           Restart
           </button> 
     </div>
